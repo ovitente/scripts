cdad979d9bc2048a2cf5c261b46c6512b3fe1a5f
Ivan Kostrubin <7spawn@gmail.com>
2021-05-27 23:03:58 +0300

    autosave

--- azp-exec.sh
+++ azp-exec.sh
@@ -1,5 +1,4 @@
 # az devops login --organization=https://dev.azure.com/YOUR_ORG/ --project=YOUR_PROJECT
 # az pipelines list  --detect --org=https://dev.azure.com/Teck/ --project=RACE21
-# az pipelines run --branch $(git rev-parse --abbrev-ref HEAD) --detect --org=https://dev.azure.com/Teck/ --project=RACE21 --name=HCA-Dispatcher-Supervisor-Backend-Build --variables DEV_DEPLOY=true --variables QA_BUILD=true --variables STAGE_BUILD=true
+az pipelines run --branch $(git rev-parse --abbrev-ref HEAD) --detect --org=https://dev.azure.com/Teck/ --project=RACE21 --name=HCA-Dispatcher-Supervisor-Backend-Build --variables DEV_BUILD=true DEV_DEPLOY=true
 # az pipelines run --branch $(git rev-parse --abbrev-ref HEAD) --detect --org=https://dev.azure.com/Teck/ --project=RACE21 --name=HCA-Dispatcher-Supervisor-Backend-Build
-az pipelines run --branch $(git rev-parse --abbrev-ref HEAD) --detect --org=https://dev.azure.com/Teck/ --project=RACE21 --name=HCA-Supervisor-Mobile-App-Build
